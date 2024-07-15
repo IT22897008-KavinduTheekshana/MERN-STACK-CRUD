0abb8e6c4b10300185a070719dbe3cf524db084b
KAVINDU <it22897008@my.sliit.lk>
2024-07-15 13:44:05 +0530

    Delete a user by ID

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -57,3 +57,17 @@ export const updateUserById = async (req, res) => {
         res.status(500).json({ errorMessage: error.message });
     }
 };
+
+
+// Delete a user by ID
+export const deleteUserById = async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        if (!deletedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { create, getAllUsers, getUserById, updateUserById } from '../controller/userController.js';
+import { create, deleteUserById, getAllUsers, getUserById, updateUserById } from '../controller/userController.js';
 
 const route = express.Router();
 
@@ -8,5 +8,6 @@ route.post('/user', create);
 route.get('/users', getAllUsers);
 route.get('/user/:id', getUserById);
 route.get('update/user/:id', updateUserById);
+route.get('delete/user/:id', deleteUserById);
 
 export default route;
