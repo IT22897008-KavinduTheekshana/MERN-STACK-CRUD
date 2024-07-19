cde35a78c7e9cccdcece5b53f2526bfc42059336
KAVINDU <it22897008@my.sliit.lk>
2024-07-19 11:51:39 +0530

    implement delete user

--- client/src/getuser/User.jsx
+++ client/src/getuser/User.jsx
@@ -1,59 +1,89 @@
-import React, { useEffect, useState } from 'react';
-import './user.css';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import './user.css'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import toast from 'react-hot-toast'
 
 const User = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/api/users');
-        setUsers(response.data);
+        const response = await axios.get('http://localhost:8000/api/users')
+        setUsers(response.data)
       } catch (error) {
-        console.log('Error while fetching data', error);
+        console.log('Error while fetching data', error)
       }
-    };
-    fetchData();
-  }, []);
+    }
+    fetchData()
+  }, [])
+
+  const deleteUser = async (userId) => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:8000/api/delete/user/${userId}`
+      )
+      setUsers((prevUsers) => prevUsers.filter((user) => user._id !== userId))
+      toast.success(response.data.message, { position: 'top-right' })
+    } catch (error) {
+      console.log(error)
+      toast.error('Error deleting user', { position: 'top-right' })
+    }
+  }
 
   return (
     <div className="userTable">
       <Link to="/add" type="button" className="btn btn-primary">
         Add User <i className="fa-solid fa-user-plus"></i>
       </Link>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th scope="col">S.No.</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Address</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => (
-            <tr key={user._id}>
-              <th scope="row">{index + 1}</th>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.address}</td>
-              <td className="actionButtons">
-                <Link to={'/update/'+user._id} type="button" className="btn btn-info">
-                  <i className="fa-solid fa-pen-to-square"></i>
-                </Link>
-                <button type="button" className="btn btn-danger">
-                  <i className="fa-solid fa-trash"></i>
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
 
-export default User;
+      {users.length === 0 ? (
+        <div className="noData">
+          <h1>No Data to display</h1>
+          <p>Please add New User</p>
+        </div>
+      ) : (
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">S.No.</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Address</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th scope="row">{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.address}</td>
+                <td className="actionButtons">
+                  <Link
+                    to={'/update/' + user._id}
+                    type="button"
+                    className="btn btn-info"
+                  >
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </Link>
+                  <button
+                    onClick={() => deleteUser(user._id)}
+                    type="button"
+                    className="btn btn-danger"
+                  >
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  )
+}
+
+export default User
